a0b07ceeff1f4540e9d5c74c8ec8e06856c72bd7
KristaB77 <87663631+KristaB77@users.noreply.github.com>
2021-08-03 07:07:43 +0300

    Add files via upload

---
+++ TableList2/TableList2/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableList2
+//
+//  Created by krista.bolsakova on 03/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
